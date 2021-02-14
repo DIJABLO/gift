fdb01884002d0adfb66217dc0d428c34c2e8902c
Diablo <riabchun.vitalii@gmail.com>
2021-02-14 19:14:22 +0200

    Commit.

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ let link = 'https://www.instagram.com/p/CK9eIrLn6ja/';
 $( "#start" ).click(function() {
     let bla = $('#post-link').val();
     if (bla === link) {
-        $( ".stats" ).removeClass( "close" );
-        $( ".stats" ).addClass( "open" );
-        $( ".bg-1" ).removeClass( "open" );
-        $( ".bg-1" ).addClass( "close" );
-        $( ".bg-2" ).removeClass( "close" );
-        $( ".bg-2" ).addClass( "open" );
+        setTimeout(function(){
+            $( ".bg-1" ).removeClass( "open" );
+            $( ".bg-1" ).addClass( "close" );
+            $( ".bg-2" ).removeClass( "close" );
+            $( ".bg-2" ).addClass( "open" );
+        }, 2000);
+        setTimeout(function(){
+            $( ".settings-form" ).removeClass( "settings-close" );
+            $( ".settings-form" ).addClass( "settings-open" );
+        }, 1000);
+
     }
 
+});
+$( "#gift-start" ).click(function() {
+    $( ".loader" ).removeClass( "loader-close" );
+    $( ".loader" ).addClass( "loaders-open" );
+    setTimeout(function(){
+        $( ".loader" ).addClass( "loader-close" );
+        $( ".result" ).removeClass( "result-close" );
+        $( ".result" ).addClass( "result-open" );
+    }, 5000);
 });
